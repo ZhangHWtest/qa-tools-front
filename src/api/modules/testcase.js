54059989d49ff1a40712eeb69f364c0acc97a487
ZhangHWtest <hbzhanghong99@163.com>
2020-05-13 14:17:14 +0800

    前端不用代理

--- src/api/modules/testcase.js
+++ src/api/modules/testcase.js
@@ -3,7 +3,7 @@ import { reques } from '../axios'
 // 获取所有项目列表
 export const getCaseList = data => {
   return reques({
-    url: '/api/case/list',
+    url: '/case/list',
     method: 'post',
     data
   })
