fe4dac70e9f3d796e8ccd42ae5e986b5f9e676a1
ZhangHWtest <hbzhanghong99@163.com>
2020-10-19 20:19:03 +0800

    线上部署

--- src/api/config.js
+++ src/api/config.js
@@ -1,5 +1,5 @@
-// const url = 'http://192.168.70.72:5000'
-const url = 'http://10.20.4.158:5555'
+const url = 'http://39.97.102.74:5001'
+// const url = 'http://10.20.4.158:5555'
 export default {
   baseUrl: url,
   headers: { 'Content-Type': 'application/json;charset=UTF-8' },
